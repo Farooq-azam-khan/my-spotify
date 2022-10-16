496e6217976b6dbb226cf49d3fa4bc43e943913a
Farooq Azam Khan <khanazamfarooq99@gmail.com>
2022-10-16 19:57:49 -0400

    integrate api route

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,23 @@
-import { PrismaClient } from '@prisma/client'
-import express, { Express, Request, Response }  from 'express'
-const prisma = new PrismaClient() 
+import express, { Express }  from 'express'
+import bodyParser  from 'body-parser'
+import dotenv from 'dotenv'
+import api_router  from './routes/api_router'
+dotenv.config()
+
 const app: Express = express()
 
 const PORT = process.env.PORT || 3001
 
 app.use(express.json())
-
-app.get('/api/v1/songs', async (req: Request, res: Response) => {
-    const songs = await prisma.song.findMany()
-    return res.status(200).json(songs)
+app.use(bodyParser.urlencoded({ extended: true }))
+app.get('/api', (req, res) => {
+    return res.json({
+        'api': ['/v1']
+    })
+})
+app.use('/api/v1', api_router)
+app.get('/', (req, res) => {
+    return res.status(200).json({'hello': 'world'})
 })
 
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`))
-export default prisma 
